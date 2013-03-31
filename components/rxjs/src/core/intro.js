e189d26de87c7b2b84fa9b3ef2b4ba0dc2321b6e
Tuomas Kareinen <tkareine@gmail.com>
2013-03-31 21:05:06 +0300

    Add RxJS and some of its support libraries

---
+++ components/rxjs/src/core/intro.js
@@ -0,0 +1,10 @@
+(function (window, undefined) {
+    var freeExports = typeof exports == 'object' && exports &&
+        (typeof global == 'object' && global && global == global.global && (window = global), exports);
+
+     /** 
+     * @name Rx
+     * @type Object
+     */
+    var Rx = { Internals: {} };
+    

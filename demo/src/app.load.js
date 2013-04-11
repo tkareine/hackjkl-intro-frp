33dc241e164a07feb43591a3ea6e4bea6eb45b15
Tuomas Kareinen <tkareine@gmail.com>
2013-04-11 21:53:00 +0300

    Show selected engine as picture

--- demo/src/app.load.js
+++ demo/src/app.load.js
@@ -3,6 +3,7 @@
 
   function load() {
     $('body').prepend($('#searchTemplate').text())
+    $('#search .engine').addClass(App.Env.engine).attr('title', 'Engine: ' + App.Env.engine)
     return Loader.loadJS('src/app.engine.' + App.Env.engine + '.js')
   }
 })(window.jQuery, window._, window.App, window.Loader)
